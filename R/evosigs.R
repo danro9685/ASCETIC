e3ea8521bdde6e5c7cb6ebfb367d0688b19fb3e7
danro9685 <daniele.ramazzotti@yahoo.com>
2023-03-30 13:23:33 +0200

    fixes

--- R/evosigs.R
+++ R/evosigs.R
@@ -2,116 +2,124 @@
 #'
 #' @examples
 #' set.seed(12345)
-#' data(aml_example)
-#' res_example_evosigs = evosigs( 
-#'                               survival_data = aml_example$survival_data, 
-#'                               evolutionary_steps = aml_example$evolutionary_steps )
+#' data(amlExample)
+#' resExampleEvosigs = evoSigs( survivalData = amlExample$survival_data, 
+#'                              evolutionarySteps = amlExample$evolutionary_steps )
 #'
-#' @title evosigs
-#' @param survival_data Survival data. Notice that no missing values are allowed.
-#' @param evolutionary_steps Evolutionary steps per patient as inferred by ASCETIC.
-#' @return A list of 3 elements: 1) survival_analysis, results from survival analysis.
-#'                               2) evolutionary_steps, evolutionary steps significantly associated with survival data.
-#'                               3) clusters_prevalence, prevalence per cluster of the selected evolutionary steps.
-#' @export evosigs
+#' @title evoSigs
+#' @param survivalData Survival data. Notice that no missing values are allowed.
+#' @param evolutionarySteps Evolutionary steps per patient as inferred by ASCETIC.
+#' @return A list of 3 elements: 1) survivalAnalysis, results from survival analysis.
+#'                               2) evolutionarySteps, evolutionary steps significantly associated with survival data.
+#'                               3) clustersPrevalence, prevalence per cluster of the selected evolutionary steps.
+#' @export evoSigs
 #' @import ggplot2
 #' @importFrom glmnet cv.glmnet
 #' @importFrom stats coef fivenum
 #' @importFrom survival Surv
 #'
-evosigs <- function( survival_data, evolutionary_steps ) {
+evoSigs <- function( survivalData, evolutionarySteps ) {
 
-    # create analyis data structure
-    analysis_data = data.frame(cbind(as.matrix(survival_data),as.matrix(evolutionary_steps)),check.rows=FALSE,stringsAsFactors=FALSE)
-    colnames(analysis_data) = c("status","times",colnames(evolutionary_steps))
+    # Create analysis data structure
+    analysisData <- data.frame(
+        cbind(
+            as.matrix(survivalData),
+            as.matrix(evolutionarySteps)),
+        check.rows = FALSE,
+        stringsAsFactors = FALSE
+    )
+    colnames(analysisData) <- c("status", "times", colnames(evolutionarySteps))
 
-    # select significant evolutionary steps based on association to survival data
-    survival_analysis = list()
-    is.valid = FALSE
-    tryCatch(expr = {
-        y = Surv(as.numeric(analysis_data$times),as.numeric(analysis_data$status))
-        lasso_cov = analysis_data[,colnames(analysis_data)[3:ncol(analysis_data)]]
-        x = as.matrix(cbind(rep(1,nrow(analysis_data)),analysis_data[,-c(1,2)]))
-        rownames(x) = 1:nrow(x)
-        colnames(x) = c("(Intercept)",paste0("lasso_cov$",colnames(lasso_cov)))
-        cv.fit = cv.glmnet(x,y,family="cox",maxit=1000000,alpha=1)
-        survival_analysis[["data"]] = analysis_data
-        survival_analysis[["fit"]] = cv.fit
-        is.valid = TRUE
+    # Select significant evolutionary steps based on association to survival data
+    survivalAnalysis <- list()
+    isValid <- FALSE
+    tryCatch({
+        y <- Surv(
+            as.numeric(analysisData$times),
+            as.numeric(analysisData$status)
+        )
+        lassoCov <- analysisData[, colnames(analysisData)[3:ncol(analysisData)]]
+        x <- as.matrix(cbind(rep(1, nrow(analysisData)), analysisData[, -c(1, 2)]))
+        rownames(x) <- 1:nrow(x)
+        colnames(x) <- c("(Intercept)", paste0("lassoCov$", colnames(lassoCov)))
+        cvFit <- cv.glmnet(x, y, family = "cox", maxit = 1000000, alpha = 1)
+        survivalAnalysis[["data"]] <- analysisData
+        survivalAnalysis[["fit"]] <- cvFit
+        isValid <- TRUE
     }, error = function(e) {})
 
-    # evaluate selected features evolutionary steps
-    if(is.valid==TRUE) {
-        curr_data = survival_analysis[["data"]]
-        curr_fit = survival_analysis[["fit"]]
-        beta_temp = matrix(NA,nrow=dim(curr_data)[1],ncol=1)
-        Coefficients = as.numeric(coef(curr_fit,s=curr_fit$lambda.min)[-1,])
-        coeff_names = gsub("lasso_cov\\$","",names(coef(curr_fit,s=curr_fit$lambda.min)[-1,]))
-        coeff_names = coeff_names[which(Coefficients!=0)]
-        Coefficients = Coefficients[which(Coefficients!=0)]
-        for(k in 1:nrow(curr_data)) {
-            x = Coefficients
-            y = curr_data[k,coeff_names,drop=FALSE]
-            beta_temp[k] = as.numeric(x%*%t(y))
+    # Evaluate selected features evolutionary steps
+    if (is.valid == TRUE) {
+        currData <- survivalAnalysis[["data"]]
+        currFit <- survivalAnalysis[["fit"]]
+        betaTemp <- matrix(NA, nrow = dim(currData)[1], ncol = 1)
+        Coefficients <- as.numeric(coef(currFit, s = currFit$lambda.min)[-1, ])
+        coeffNames <- gsub("lasso_cov\\$", "", names(Coefficients))
+        coeffNames <- coeffNames[which(Coefficients != 0)]
+        Coefficients <- Coefficients[which(Coefficients != 0)]
+        for (k in 1:nrow(currData)) {
+            x <- Coefficients
+            y <- currData[k, coeffNames, drop = FALSE]
+            betaTemp[k] <- as.numeric(x %*% t(y))
         }
-        beta = as.numeric(beta_temp)
-        survival_analysis$beta = beta
-        significant_coefficients = list()
-        coeff_pos = as.numeric(Coefficients[which(Coefficients>0)])
-        names(coeff_pos) = coeff_names[which(Coefficients>0)]
-        coeff_neg = as.numeric(Coefficients[which(Coefficients<0)])
-        names(coeff_neg) = coeff_names[which(Coefficients<0)]
-        significant_coefficients[["positive"]] = coeff_pos
-        significant_coefficients[["negative"]] = coeff_neg
-        survival_analysis$significant_coefficients = significant_coefficients
-        range = sort(unique(fivenum(survival_analysis$beta)[2:4]))
-        survival_analysis$clusters = rep("C1",length(beta))
-        if(length(range)==1) {
-            survival_analysis$clusters[survival_analysis$beta>=range] = "C2"
+        beta <- as.numeric(betaTemp)
+        survivalAnalysis$beta <- beta
+        significantCoefficients <- list()
+        coeffPos <- as.numeric(Coefficients[which(Coefficients > 0)])
+        names(coeffPos) <- coeffNames[which(Coefficients > 0)]
+        coeffNeg <- as.numeric(Coefficients[which(Coefficients < 0)])
+        names(coeffNeg) <- coeffNames[which(Coefficients < 0)]
+        significantCoefficients[["positive"]] <- coeffPos
+        significantCoefficients[["negative"]] <- coeffNeg
+        survivalAnalysis$significantCoefficients <- significantCoefficients
+        range <- sort(unique(fivenum(survivalAnalysis$beta)[2:4]))
+        survivalAnalysis$clusters <- rep("C1", length(beta))
+        if (length(range) == 1) {
+            survivalAnalysis$clusters[survivalAnalysis$beta >= range] <- "C2"
         }
-        if(length(range)==2) {
-            survival_analysis$clusters[survival_analysis$beta>=range[1]] = "C2"
-            survival_analysis$clusters[survival_analysis$beta>=range[2]] = "C3"
+        if (length(range) == 2) {
+            survivalAnalysis$clusters[survivalAnalysis$beta >= range[1]] <- "C2"
+            survivalAnalysis$clusters[survivalAnalysis$beta >= range[2]] <- "C3"
         }
-        if(length(range)==3) {
-            survival_analysis$clusters[survival_analysis$beta>=range[1]] = "C2"
-            survival_analysis$clusters[survival_analysis$beta>=range[2]] = "C3"
-            survival_analysis$clusters[survival_analysis$beta>=range[3]] = "C4"
+        if (length(range) == 3) {
+            survivalAnalysis$clusters[survivalAnalysis$beta >= range[1]] <- "C2"
+            survivalAnalysis$clusters[survivalAnalysis$beta >= range[2]] <- "C3"
+            survivalAnalysis$clusters[survivalAnalysis$beta >= range[3]] <- "C4"
         }
-        clusters = rep(NA,length(beta))
-        clust_pos = 0
-        for(curr_clust_pos in sort(unique(survival_analysis$clusters))) {
-            clust_pos = clust_pos + 1
-            clusters[which(survival_analysis$clusters==curr_clust_pos)] = paste0("C",clust_pos)
+        clusters <- rep(NA, length(beta))
+        clustPos <- 0
+        for (currClustPos in sort(unique(survivalAnalysis$clusters))) {
+            clustPos <- clustPos + 1
+            clusters[which(survivalAnalysis$clusters == currClustPos)] <- paste0("C", clustPos)
         }
-        survival_analysis$clusters = NULL
-        survival_analysis$clusters = clusters
+        survivalAnalysis$clusters <- NULL
+        survivalAnalysis$clusters <- clusters
     }
-    if(length(unique(survival_analysis$clusters))<2) {
-        is.valid = FALSE
+    if (length(unique(survivalAnalysis$clusters)) < 2) {
+        is.valid <- FALSE
     }
 
-    # save the extracted evolutionary signatures
-    evolutionary_signatures = list()
-    if(is.valid==FALSE) {
+    # Save the extracted evolutionary signatures
+    evolutionarySignatures <- list()
+    if (isValid == FALSE) {
         message("No evolutionary step could be significantly associated to differences in prognosis...","\n")
     }
-    if(is.valid==TRUE) {
-        survival_analysis = survival_analysis
-        selected_evolutionary_steps = c(survival_analysis$significant_coefficients$positive,survival_analysis$significant_coefficients$negative)
-        selected_evolutionary_steps = selected_evolutionary_steps[sort(unique(names(selected_evolutionary_steps)))]
-        clusters = survival_analysis$clusters
-        freq_features = array(NA,c(length(unique(clusters)),length(evolutionary_steps)))
-        rownames(freq_features) = sort(unique(clusters))
-        colnames(freq_features) = sort(names(evolutionary_steps))
-        for(i in rownames(freq_features)) {
-            curr_samples_data = evolutionary_steps[which(clusters==i),,drop=FALSE]
-            curr_estimate = colSums(curr_samples_data)/nrow(curr_samples_data)
-            freq_features[i,names(curr_estimate)] = as.numeric(curr_estimate)
+    if (isValid == TRUE) {
+        survivalAnalysis <- survivalAnalysis
+        selectedEvolutionarySteps <- c(survivalAnalysis$significantCoefficients$positive, survivalAnalysis$significantCoefficients$negative)
+        selectedEvolutionarySteps <- selectedEvolutionarySteps[sort(unique(names(selectedEvolutionarySteps)))]
+        clusters <- survivalAnalysis$clusters
+        freqFeatures <- array(NA, c(length(unique(clusters)), length(evolutionarySteps)))
+        rownames(freqFeatures) <- sort(unique(clusters))
+        colnames(freqFeatures) <- sort(names(evolutionarySteps))
+        for (i in rownames(freqFeatures)) {
+            currSamplesData <- evolutionarySteps[which(clusters==i), , drop=FALSE]
+            currEstimate <- colSums(currSamplesData) / nrow(currSamplesData)
+            freqFeatures[i, names(currEstimate)] <- as.numeric(currEstimate)
         }
-        evolutionary_signatures = list(survival_analysis=survival_analysis,evolutionary_steps=selected_evolutionary_steps,clusters_prevalence=freq_features)
+        evolutionarySignatures <- list(survivalAnalysis=survivalAnalysis, evolutionarySteps=selectedEvolutionarySteps, clustersPrevalence=freqFeatures)
     }
 
-    return(evolutionary_signatures)
+    return(evolutionarySignatures)
 
 }
