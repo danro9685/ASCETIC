39752a3f16261b0dfe3f4054b98c5d5d16a2d4a2
luca-dex <luca.desano@gmail.com>
2023-04-04 19:48:43 +0200

    remove print to stderr

--- src/balance.cpp
+++ src/balance.cpp
@@ -312,9 +312,6 @@ balance(network & g, residual & r, double delta)
 		//printf("%d %d\n", source_cnt, s->id);
 		if (TAILQ_EMPTY(&sinks) || source_cnt == 0) break;
 
-		//if (source_cnt % 10 == 0)
-			//fprintf(stderr, "imbalanced nodes: %d  \r", source_cnt);
-
 		processworkset(r, &workset, &affected);
 		processaffected(r, &affected, &stack);
 	}
